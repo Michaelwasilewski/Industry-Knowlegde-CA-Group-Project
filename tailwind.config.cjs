14c53b8ab85cb620ad3272a23421d0a76eb195cf
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2023-01-16 14:14:54 +0100

    Added colors, font-sizes and font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,36 @@
 module.exports = {
   content: ['./src/*.{html,js}', './src/js/*.js'],
   theme: {
-    extend: {},
+    fontSize: {
+      'body': ['18px', {
+        lineHeight: '28px',
+        fontWeight: '500',
+      }],
+      'smallHeader': ['32px', {
+        lineHeight: 'leading-normal',
+        fontWeight: '700',
+      }],
+      'mediumHeader': ['40px', {
+        lineHeight: 'leading-normal',
+        fontWeight: '700',
+      }],
+      'largeHeader': ['56px', {
+        lineHeight: 'leading-normal',
+        fontWeight: '700',
+      }]
+    },
+    extend: {
+      colors: {
+        mediumBrown: '#7A746E',
+        lightCream: '#FFF7F0',
+        galacticBlue: '#755CDE',
+        summerYellow: '#F6A560',
+        pink: '#F39E9E',
+        lightRed: '#EB7565',
+        cyan: '#755CDE',
+        darkPurple: '#552049'
+      }
+    },
   },
   plugins: [],
 };
